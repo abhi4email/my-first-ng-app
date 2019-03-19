0f0566b8d4d780e46766373e7f7ac9a3d41b955c
abhishek <abhi4email@gmail.com>
2019-03-19 09:48:29 +0530

    init project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('my-first-ng-app');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title in a h2 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-first-ng-app!');
+    expect(compiled.querySelector('h2').textContent).toContain('Welcome to my-first-ng-app!');
   });
 });
